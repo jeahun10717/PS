db9eeaf7cc0ae0b56547a8a44d50c63397a17ddc
jeahun10717 <jeahun10717@gmail.com>
2022-05-27 18:50:12 +0900

    ps upload

--- algorithm_bj/algorithm_bj/main.cpp
+++ algorithm_bj/algorithm_bj/main.cpp
@@ -1,48 +1,18 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int T, x1, y1, r1, x2, y2, r2;
+vector <int> vecArr[17];
+vector <int> v;
+int arr[17];
 
-int chkCrossPoint(int x1, int y1, int r1, int x2, int y2, int r2){
-    int maxRad = max(r1, r2);
-    maxRad = maxRad*maxRad;
-    int dist = (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
-    if(x1 == x2 && y1 == y2 && r1 == r2){ // 무한개의 점이 나올 경우
-        return -1;
-    }
-    if(maxRad > dist){
-        if(dist < (r1 - r2) * (r1 - r2)){
-            return 0;
-        }else if(dist == (r1 - r2) * (r1 - r2)){
-            return 1;
-        }else if(dist > (r1 - r2) * (r1 - r2)){
-            return 2;
-        }
-    }else if(maxRad < dist){
-        if(dist > (r1 + r2) * (r1 + r2)){
-            return 0;
-        }else if(dist == (r1 + r2) * (r1 + r2)){
-            return 1;
-        }else if(dist < (r1 + r2) * (r1 + r2)){
-            return 2;
-        }
-    }else if(maxRad == dist){
-        return 2;
-    }
-    return -1;
-}
-
-int main() {
+int main(void){
     
-    cin >> T;
-    
-    for (int i = 0; i < T; i++) {
-        cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
-        cout << chkCrossPoint(x1, y1, r1, x2, y2, r2) << '\n';
-    }
-    
+    cout << sizeof(int) << '\n';
+    cout << sizeof(arr) << '\n';
+    cout << sizeof(vecArr) << '\n';
+    cout << sizeof(v) << '\n';
     
     return 0;
 }
