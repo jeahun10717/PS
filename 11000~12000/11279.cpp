56be12c83f85d54e49da151d9cf715e1be638c82
jeahun10717 <jeahun10717@gmail.com>
2022-06-07 16:52:00 +0900

    ps upload

--- 11000~12000/11279.cpp
+++ 11000~12000/11279.cpp
@@ -12,8 +12,17 @@ int main(void){
     
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    
-    
-    
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> x;
+        if(pq.empty() && x == 0) cout << 0 << '\n';
+        else if(x == 0){
+            cout << pq.top() << '\n';
+            pq.pop();
+        }
+        else pq.push(x);
+    }
+
     return 0;
 }
